f9888a584e37e7fb8acdab5b1956a87fde466a0a
CortinaRx <cortinarcz@gmail.com>
2022-01-25 23:23:44 +0100

    3r commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 })
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:4200'],
+    origin: ['https://destinos-angular.vercel.app', 'http://localhost:4200'],
     credentials: true
 }))
 
